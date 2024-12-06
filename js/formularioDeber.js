82177432c6d490d6b62308c238a770559a1c1890
mapinedaf <mapinedaf@uce.edu.ec>
2024-12-06 16:24:40 -0500

    "test"

---
+++ js/formularioDeber.js
@@ -0,0 +1,47 @@
+const personas = [
+    {
+        nombre: "Karl",
+        apellido:"Marx",
+        hobby:"Escribir",
+        lugarNacimiento:"Prusia"
+    },
+    {
+        nombre: "Firederich",
+        apellido:"Engels",
+        hobby:"Filosofía",
+        lugarNacimiento:"Prusia"
+    }
+]
+
+
+
+const app = Vue.createApp({
+
+
+    methods: {
+
+        guardarPersona(){
+            console.log("Metodo guardar persona")
+            this.listaPersonas.unshift(
+                {
+                    nombre:this.inputNombre,
+                    apellido:this.inputApellido,
+                    hobby:this.inputHobby,
+                    lugarNacimiento:this.inputLugarNacimiento
+                }
+            )
+        }
+    },
+    data() {
+        return {
+            listaPersonas: personas,
+            inputNombre:"",
+            inputApellido:"",
+            inputHobby:"",
+            inputLugarNacimiento:""
+        }
+    }
+
+})
+
+app.mount('#myApp');
